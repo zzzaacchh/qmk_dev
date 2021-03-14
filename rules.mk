25868b15f05c258817cbed9ac9b15fed3cc2526b
Zach <38481977+zzzaacchh@users.noreply.github.com>
2021-03-14 14:17:19 -0500

    Add files via upload

---
+++ rules.mk
@@ -0,0 +1,6 @@
+MIDI_ENABLE         = yes   # MIDI controls
+AUDIO_ENABLE        = yes   # Audio output on port C6
+TERMINAL_ENABLE     = yes   # QMK Terminal
+UNICODEMAP_ENABLE   = yes   # Unicode character array
+DYNAMIC_MACRO_ENABLE = yes  # Dynamic Macros
+BOOTLOADER = stm32-dfu
